c30c7c6c841a8e2403f7327a372727127757a213
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 <vaidikbhesaniya1212@gmail.com>
2025-09-06 13:24:41 +0530

    06/09/25

--- daily.sql
+++ daily.sql
@@ -37,6 +37,15 @@ SELECT score,
        DENSE_RANK() OVER (ORDER BY score DESC) AS rank
 FROM Scores
 
+
+
+--Find all numbers that appear at least three times consecutively.
+
+--Return the result table in any order.
+
+--The result format is in the following example.
+
+
 -- Create the Logs table
 CREATE TABLE Logs (
     id INT PRIMARY KEY,
@@ -52,3 +61,101 @@ INSERT INTO Logs (id, num) VALUES
 (5, '1'),
 (6, '2'),
 (7, '2');
+
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs l1
+JOIN Logs l2 ON l1.id = l2.id - 1 AND l1.num = l2.num
+JOIN Logs l3 ON l2.id = l3.id - 1 AND l2.num = l3.num;
+
+
+
+
+
+
+--06 09 2024
+
+
+--Write a solution to find employees who have the highest salary in each of the departments.
+
+--Return the result table in any order.
+
+--The result format is in the following example.
+
+ 
+
+
+
+
+-- Create Department table
+CREATE TABLE Department (
+    id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+-- Insert values into Department table
+INSERT INTO Department (id, name) VALUES
+(1, 'IT'),
+(2, 'Sales');
+
+-- Create Employee table
+CREATE TABLE Employee (
+    id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    salary INT NOT NULL,
+    departmentId INT,
+    FOREIGN KEY (departmentId) REFERENCES Department(id)
+);
+
+-- Insert values into Employee table
+INSERT INTO Employee (id, name, salary, departmentId) VALUES
+(1, 'Joe', 70000, 1),
+(2, 'Jim', 90000, 1),
+(3, 'Henry', 80000, 2),
+(4, 'Sam', 60000, 2),
+(5, 'Max', 90000, 1);
+
+
+select * from employee
+
+select * from department
+
+SELECT d.name AS Department,
+       e.name AS Employee,
+       e.salary AS Salary
+FROM Employee e
+JOIN Department d
+    ON e.departmentId = d.id
+WHERE e.salary = (
+    SELECT MAX(salary)
+    FROM Employee
+    WHERE departmentId = e.departmentId
+);
+
+
+
+--Write a solution to delete all duplicate emails, keeping only one unique email with the smallest id.
+
+-- Create Person table
+CREATE TABLE Person (
+    id INT PRIMARY KEY,
+    email VARCHAR(100) NOT NULL
+);
+drop table person
+
+-- Insert sample data
+INSERT INTO Person (id, email) VALUES
+(1, 'john@example.com'),
+(2, 'bob@example.com'),
+(3, 'john@example.com');
+
+-- Check inserted data
+SELECT * FROM Person;
+
+DELETE FROM Person
+WHERE id NOT IN (
+    SELECT id FROM (
+        SELECT MIN(id) AS id
+        FROM Person
+        GROUP BY email
+    ) AS temp
+);
