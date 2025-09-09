b23fe5c0193b573f979267d5d4da3e67aedc786f
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 <vaidikbhesaniya1212@gmail.com>
2025-09-09 11:24:54 +0530

    09/09/25

--- daily.sql
+++ daily.sql
@@ -286,3 +286,29 @@ SELECT * FROM Customer;
 SELECT name
 FROM Customer
 WHERE referee_id <> 2 OR referee_id IS NULL;
+
+
+
+--09-09-2004
+
+create database ninenine
+
+use ninenine
+
+
+CREATE TABLE Orders (
+    order_number INT PRIMARY KEY,
+    customer_number INT
+);
+
+INSERT INTO Orders (order_number, customer_number) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 3);
+
+
+SELECT Top 1 customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(order_number) DESC;
