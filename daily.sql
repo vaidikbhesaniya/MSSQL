5eac53afcea22ebacfcef471ce0ebfe6c0ef1cc1
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 <vaidikbhesaniya1212@gmail.com>
2025-09-15 15:51:27 +0530

    15/09/25

--- daily.sql
+++ daily.sql
@@ -568,3 +568,36 @@ FROM Project p
 JOIN Employee e 
     ON p.employee_id = e.employee_id
 GROUP BY p.project_id;
+
+
+--15/09/25
+
+create database nine
+CREATE TABLE Activity (
+    user_id INT NOT NULL,
+    session_id INT NOT NULL,
+    activity_date DATE NOT NULL,
+    activity_type VARCHAR(20) NOT NULL
+);
+
+INSERT INTO Activity (user_id, session_id, activity_date, activity_type) VALUES
+(1, 1, '2019-07-20', 'open_session'),
+(1, 1, '2019-07-20', 'scroll_down'),
+(1, 1, '2019-07-20', 'end_session'),
+(2, 4, '2019-07-20', 'open_session'),
+(2, 4, '2019-07-21', 'send_message'),
+(2, 4, '2019-07-21', 'end_session'),
+(3, 2, '2019-07-21', 'open_session'),
+(3, 2, '2019-07-21', 'send_message'),
+(3, 2, '2019-07-21', 'end_session'),
+(4, 3, '2019-06-25', 'open_session'),
+(4, 3, '2019-06-25', 'end_session');
+
+select *  from Activity
+
+SELECT 
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27'
+GROUP BY activity_date;
